65e83a6edf4cdbef7df9a4d26ecc9f378256b579
ValeraVasilevsky <valera200208@gmail.com>
2024-08-21 16:57:45 +0700

    feat: added router

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <main>
+    <RouterView />
+  </main>
 </template>
-
-<script setup lang="ts"></script>
---
+++ src/app/router.ts
@@ -0,0 +1,34 @@
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+import type { Component } from "vue";
+
+export const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      component: (): Component => import("pages/layout.vue"),
+      children: [
+        {
+          path: "",
+          name: "posts",
+          component: (): Component => import("pages/posts/index.vue"),
+        },
+      ],
+    },
+    {
+      path: "/",
+      component: (): Component => import("pages/default.vue"),
+      children: [
+        {
+          path: "/:catchAll(.*)",
+          name: "404",
+          component: (): Component => import("pages/NotFound.vue"),
+        },
+      ],
+    },
+  ] as RouteRecordRaw[],
+});
