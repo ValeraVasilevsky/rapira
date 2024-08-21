71ffb71d05877487062be56bb214b60ad739dc72
ValeraVasilevsky <valera200208@gmail.com>
2024-08-21 16:49:48 +0700

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
+import svgLoader from "vite-svg-loader";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+
+    svgLoader({
+      defaultImport: "component",
+      svgoConfig: {
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                removeViewBox: false,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      app: fileURLToPath(new URL("./src/app", import.meta.url)),
+      features: fileURLToPath(new URL("./src/features", import.meta.url)),
+      shared: fileURLToPath(new URL("./src/shared", import.meta.url)),
+      entities: fileURLToPath(new URL("./src/entities", import.meta.url)),
+      widgets: fileURLToPath(new URL("./src/widgets", import.meta.url)),
+      pages: fileURLToPath(new URL("./src/pages", import.meta.url)),
+    },
+  },
+
+  server: {
+    port: 8080,
+  },
+});
